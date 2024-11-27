972e1982e1ef013567fb0873405dd140166ba274
lvspa <166564473+lvspa@users.noreply.github.com>
2024-11-27 17:50:03 -0300

    teste de commit

---
+++ SoulFitApplication.java
@@ -0,0 +1,13 @@
+package br.com.SoulFit.SoulFit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulFitApplication.class, args);
+	}
+
+}
